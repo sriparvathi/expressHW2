26214aaaf7ec084a7c6f4c54808c301070488543
parvathi <sriparvathi.kalakoti@gmail.com>
2022-11-30 12:34:05 -0600

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ app.get('/greeting', function(req, res){
 app.get('/greeting/:name', function(req, res){
     res.send('What\'s up ' + req.params.name);
 });
+// write arrow function instead of conventional function
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+     console.log("hello");
+     //res.send("here is your tip");
+     let result= (req.params.total * (req.params.tipPercentage / 100));
+    res.send(`${result}`);
+});
 //Tell the app or server to listen for requests or port
 let port = 3000;
 app.listen(port, function(){
