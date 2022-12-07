881b0e807ebc43e6b9fa6d61161af05ecf647c25
parvathi <sriparvathi.kalakoti@gmail.com>
2022-12-07 08:05:23 -0600

    with corrections

--- server.js
+++ server.js
@@ -20,14 +20,16 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.send(`${result}`);
 });
 //Magic 8 Ball
-app.get('/magic/Will%20I%20Be%20A%20Millionaire', (req, res) => {
+app.get('/magic/:question', (req, res) => {
     let arr = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"] 
-    let randomQuote = (Math.floor(Math.random()*arr.length));
-    res.send (`Will%20I%20Be%20A%20Millionaire ${arr[randomQuote]}`);
+    let randomQuote = arr[Math.floor(Math.random()*arr.length)];
+    //console.log(randomQuote);
+    let question = req.params.question;
+    res.send(question + "?"+`<h1>${randomQuote}</h1>`);
+   
 });
 //Tell the app or server to listen for requests or port
 let port = 3000;
 app.listen(port, function(){
     console.log(`Listening on ${port}`);
 });
-
